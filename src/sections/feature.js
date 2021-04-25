08a94759400dc694a22a79de532c9dc546af9fcb
Goozi <goozionline@gmail.com>
2021-04-25 18:04:57 +0100

    updated how to buy

--- src/sections/feature.js
+++ src/sections/feature.js
@@ -106,6 +106,7 @@ export default function Feature() {
               }}
               variant='outlinePrimary'
               aria-label='Get Started'
+              id="contract-address"
             >
               <Container
                 sx={{
